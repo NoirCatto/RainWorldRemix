754f573f3bac1094fb7c5ea45c08c16f676f95ec
NoirCatto <noircatto@catspace.dev>
2025-12-04 20:18:28 +0100

    Initial Commit (WormRepellent)

---
+++ WormRepellent/README.md
@@ -0,0 +1,11 @@
+# Worm Repellent
+
+Description : <br>
+Prevents garbage worms from stealing your spears. <br>
+Tribute to a legacy mod with the same name
+***
+[Steam Workshop Link](https://steamcommunity.com/sharedfiles/filedetails/?id=2976287804)
+<br>
+[ZIP Release](https://github.com/NoirCatto/RainWorldRemix/raw/master/ExplodingBatflies/(DOWNLOAD%20THIS)%20ExplodingBatflies.zip)
+# Non-Workshop Installation
+If you're on a non-steam installation of the game, get the Worm Repellent zip and copy the folder inside into `Rain World/RainWorld_Data/StreamingAssets/mods`
---
+++ WormRepellent/WormRepellent.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security;
+using System.Security.Permissions;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using RWCustom;
+using BepInEx;
+using BepInEx.Logging;
+using MonoMod.Cil;
+using MoreSlugcats;
+using Debug = UnityEngine.Debug;
+
+#pragma warning disable CS0618
+
+[module: UnverifiableCode]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+
+namespace WormRepellent;
+
+[BepInPlugin("NoirCatto.WormRepellent", "WormRepellent", "1.0.0")]
+public partial class WormRepellent : BaseUnityPlugin
+{
+    public void OnEnable()
+    {
+        On.RainWorld.OnModsInit += RainWorldOnOnModsInit;
+    }
+
+    public bool IsInit;
+    public void RainWorldOnOnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+    {
+        orig(self);
+        try
+        {
+            if (IsInit) return;
+            
+            On.GarbageWorm.ctor += GarbageWormOnctor;
+            On.GarbageWorm.Extend += GarbageWormOnExtend;
+            
+            IsInit = true;
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError(ex);
+        }
+    }
+
+    public void GarbageWormOnExtend(On.GarbageWorm.orig_Extend orig, GarbageWorm self)
+    {
+        orig(self);
+        self.grabSpears = false;
+    }
+
+    public void GarbageWormOnctor(On.GarbageWorm.orig_ctor orig, GarbageWorm self, AbstractCreature abstractCreature, World world)
+    {
+        orig(self, abstractCreature, world);
+        self.grabSpears = false;
+    }
+}
